e41deed540c82ccf7d36584c9c51d3fb7055ad4b
Mihai Bura <mihai.bura@qliqup.com>
2012-08-24 11:42:49 -0500

    Updated path, css and js

--- script.js
+++ script.js
@@ -16,17 +16,17 @@
 							}
 						}
 
-						$("#json_content").append("<div class=\"animated_widget_deal\"></div>").append("<table></table>").append("<tr><th colspan='3'> " + deal.text + " </th></tr><tr><td> Deals Left </td><td> Deals Redeemed </td><td> QP Cost </td></tr>").append("<tr></tr>").append("<td>" + dealsLeft + "</td>").append("<td>" + deal.redeemed + "</td>").append("<td>" + deal.qpoint_cost + "</td>");
+						$("#qliq_json_content").append("<div class=\"qliq_animated_widget_deal\"></div>").append("<table></table>").append("<tr><th colspan='3'> " + deal.text + " </th></tr><tr><td> Deals Left </td><td> Deals Redeemed </td><td> QP Cost </td></tr>").append("<tr></tr>").append("<td>" + dealsLeft + "</td>").append("<td>" + deal.redeemed + "</td>").append("<td>" + deal.qpoint_cost + "</td>");
 					}
 				});
 
-				$("#animated_widget_open_trigger").click(function() {
+				$("#qliq_animated_widget_open_trigger").click(function() {
 					$(this).hide(100);
-					$("#animated_widget_content").show(200);
+					$("#qliq_animated_widget_content").show(200);
 				});
 
-				$("#animated_widget_close_trigger").click(function() {
-					$("#animated_widget_content").hide(200);
-					$("#animated_widget_open_trigger").show(100);
+				$("#qliq_animated_widget_close_trigger").click(function() {
+					$("#qliq_animated_widget_content").hide(200);
+					$("#qliq_animated_widget_open_trigger").show(100);
 				});
 			});
