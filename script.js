82da7e86af1352aea2e39a049cd9ad17312d2047
Mihai Bura <mihai.bura@qliqup.com>
2012-08-30 16:50:26 -0500

    Link fix

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ s = d.createElement('script')
 
 b = d.getElementsByTagName('body')
 
-$('body').append('<div class="qliq_animated_widget_holder"><div id="qliq_animated_widget_container"><div id="qliq_animated_widget_content"><div id="qliq_business_spacer"></div><div id="qliq_business_title_holder"></div><div id="qliq_json_content"></div><div class="qliq_animated_widget_download">Get these deals now! Download Qliq on <a href="">iPhone</a> or <a href="">Android</a><br/>Learn more at <a href="http://qliqup.com">www.QliqUp.com</a></div> </div><div id="qliq_animated_widget_open_trigger"><div id="qliq_trigger_left"><img src="http://qliqup.com/media/images/dealtag.png" alt=""/></div> <div id="qliq_trigger_center">Qliq Deals!</div><div id="qliq_trigger_right"><img style="display: none;" id="arrow_up" src="http://qliqup.com/media/images/circlearrow_up.png" alt=""/></div><div style="clear: both"></div> </div> </div>')
+$('body').append('<div class="qliq_animated_widget_holder"><div id="qliq_animated_widget_container"><div id="qliq_animated_widget_content"><div id="qliq_business_spacer"></div><div id="qliq_business_title_holder"></div><div id="qliq_json_content"></div><div class="qliq_animated_widget_download">Get these deals now! Download Qliq on <a href="http://tinyurl.com/Qliq-iPhone" target="_blank">iPhone</a> or <a href="https://play.google.com/store/apps/details?id=com.qliqup.android" target="_blank">Android</a><br/>Learn more at <a href="http://qliqup.com" target="_blank">www.QliqUp.com</a></div> </div><div id="qliq_animated_widget_open_trigger"><div id="qliq_trigger_left"><img src="http://qliqup.com/media/images/dealtag.png" alt=""/></div> <div id="qliq_trigger_center">Qliq Deals!</div><div id="qliq_trigger_right"><img style="display: none;" id="arrow_up" src="http://qliqup.com/media/images/circlearrow_up.png" alt=""/></div><div style="clear: both"></div> </div> </div>')
 
 $(document).ready(function() {
 	$.getJSON('http://test.qliqup.com/v1.2/qliqserver/public/deals/' + QLIQ_ID + '/?callback=?', function(data) {
 		var deals = data.data.deals;
 		var businessName = data.data.deals[0].name;
 		
-		$("#qliq_business_title_holder").html("<a href='http://qliqup.com'>" + businessName+ " has " + deals.length + " deals on Qliq</a>");
+		$("#qliq_business_title_holder").html("<a href='http://qliqup.com' target='_blank'>" + businessName+ " has " + deals.length + " deals on Qliq</a>");
 		for(var i = 0; i < deals.length; i++) {
 			var deal = deals[i].data;
 			var dealsLeft, dealsRedeemed, qpointCost;
@@ -37,7 +37,7 @@ $(document).ready(function() {
 			$("#qliq_json_content").html(
 							$("#qliq_json_content").html()
 							+"<table>"
-							+ "<tr><td colspan='3' class='qliq_deal_title'><a href='http://qliqup.com'>" + deal.text + "</a></td></tr>"
+							+ "<tr><td colspan='3' class='qliq_deal_title'><a href='http://qliqup.com' target='_blank'>" + deal.text + "</a></td></tr>"
 							+ "<tr>"
 							+ "<td>" + dealsLeft + " Deals Left | "
 							+ deal.qpoint_cost + " QPoints</td>"
