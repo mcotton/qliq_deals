da12da3e8dd5bf63accd4d8c53eea2aff8fd9f50
Mihai Bura <mihai.bura@qliqup.com>
2012-08-30 16:00:35 -0500

    New version of Qliq Deals Widget

--- script2.js
+++ script2.js
@@ -1,15 +1,15 @@
 d = document
 
-var ss = document.createElement("link");
-ss.type = "text/css";
-ss.rel = "stylesheet";
-ss.href = "http://test.qliqup.com/future/static/site/qliq_deals/style.css";
-document.getElementsByTagName("head")[0].appendChild(ss);
+//var ss = document.createElement("link");
+//ss.type = "text/css";
+//ss.rel = "stylesheet";
+//ss.href = "http://test.qliqup.com/future/static/site/qliq_deals/style.css";
+//document.getElementsByTagName("head")[0].appendChild(ss);
 
 
 s = d.createElement('script')
 
 b = d.getElementsByTagName('body')
 
-$('body').append('<div class="qliq_animated_widget_holder"><div id="qliq_animated_widget_container"><div id="qliq_animated_widget_content"><div id="qliq_animated_widget_header"><img src="http://qliqup.com/merchants/images/icons/small/Qliq_TRADITIONAL_small_option4.png" alt=""/></div><div id="qliq_json_content"></div><div class="qliq_animated_widget_download"><a target="_blank" href="http://tinyurl.com/Qliq-iPhone"> <img src="http://qliqup.com/media/images/app_store_badge.png" alt="iPhone"></a><a target="_blank" href="https://play.google.com/store/apps/details?id=com.qliqup.android"> <img src="http://qliqup.com/media/images/google_play_badge.png" alt="iPhone"> </a></div> <div id="qliq_animated_widget_close_trigger"><img src="http://qliqup.com/media/images/arrow_left.png" alt=""/> </div></div><div id="qliq_animated_widget_open_trigger"><table><tr><td valign="center">Check out our Qliq Deals</td><td valign="center"><img src="http://qliqup.com/media/images/arrow_right.png" alt=""/></td></tr> </table></div><div style="clear: both"></div> </div> </div>')
+$('body').append('<div class="qliq_animated_widget_holder"><div id="qliq_animated_widget_container"><div id="qliq_animated_widget_content"><div id="qliq_business_spacer"></div><div id="qliq_business_title_holder"></div><div id="qliq_json_content"></div><div class="qliq_animated_widget_download">Get these deals now! Download Qliq on <a href="">iPhone</a> or <a href="">Android</a><br/>Learn more at <a href="http://qliqup.com">www.QliqUp.com</a></div> </div><div id="qliq_animated_widget_open_trigger"><div id="qliq_trigger_left"><img src="http://qliqup.com/media/images/dealtag.png" alt=""/></div> <div id="qliq_trigger_center">Qliq Deals!</div><div id="qliq_trigger_right"><img style="display: none;" id="arrow_up" src="http://qliqup.com/media/images/circlearrow_up.png" alt=""/></div><div style="clear: both"></div> </div> </div>')
 
