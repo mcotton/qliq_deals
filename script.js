ef1247cb9500aea69571f8c507b0fba64f1dc724
Mark Cotton <tifosigoon@gmail.com>
2012-09-28 13:59:01 -0500

    changes for sams burger joint

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 (function($) {
 
-d = document
+var d = document
+var s = d.createElement('script')
+var b = d.getElementsByTagName('body')
 
 var ss = document.createElement("link");
 ss.type = "text/css";
@@ -9,14 +11,10 @@ ss.href = "http://static.qliqup.com/qliq_deals/style.css";
 document.getElementsByTagName("head")[0].appendChild(ss);
 
 
-s = d.createElement('script')
-
-b = d.getElementsByTagName('body')
-
 $('body').append('<div class="qliq_animated_widget_holder"><div id="qliq_animated_widget_container"><div id="qliq_animated_widget_content"><div id="qliq_business_spacer"></div><div id="qliq_business_title_holder"></div><div id="qliq_json_content"></div><div class="qliq_animated_widget_download">Get these deals now! Download Qliq on <a href="http://tinyurl.com/Qliq-iPhone" target="_blank">iPhone</a> or <a href="https://play.google.com/store/apps/details?id=com.qliqup.android" target="_blank">Android</a><br/>Learn more at <a href="http://qliqup.com" target="_blank">www.QliqUp.com</a></div> </div><div id="qliq_animated_widget_open_trigger"><div id="qliq_trigger_left"><img src="http://qliqup.com/media/images/dealtag.png" alt=""/></div> <div id="qliq_trigger_center">Qliq Deals!</div><div id="qliq_trigger_right"><img style="display: none;" id="arrow_up" src="http://qliqup.com/media/images/circlearrow_up.png" alt=""/><img id="arrow_down" src="http://qliqup.com/media/images/circlearrow_down.png" alt=""/></div><div style="clear: both"></div> </div> </div>')
 
 $(document).ready(function() {
-	$.getJSON('http://test.qliqup.com/v1.2/qliqserver/public/deals/' + QLIQ_ID + '/?callback=?', function(data) {
+	$.getJSON('http://api.qliqup.com/v1.2/qliqserver/public/deals/' + QLIQ_ID + '/?callback=?', function(data) {
 		var deals = data.data.deals;
 		var businessName = data.data.deals[0].name;
 		
